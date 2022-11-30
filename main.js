0f6f6021bb914bbc6146a2c2dc9f18988481c1b0
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2022-12-01 00:27:56 +0400

    changed substring method to slice method

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ console.log(internationalize);
 const modernizeNum = (number) =>
   number.length !== 9 || !/^-?\d+$/.test(number)
     ? `Please enter exactly nine digits ${number.length}`
-    : `${number.substring(0, 3)}-${number.substring(3, 5)}-${number.substring(
+    : `${number.slice(0, 3)}-${number.slice(3, 5)}-${number.slice(
         5,
         7
-      )}-${number.substring(7, 9)}`;
+      )}-${number.slice(7, 9)}`;
 
 console.log(modernizeNum(numbers));
